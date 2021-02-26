b0ecb1d475acfb189457a73fb1106ff1f722f713
prioritize <leach.jared@gmail.com>
2021-02-26 12:45:18 -0800

    Continuing structure update

--- Example.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright: 2020 -- Jared Leach
-#ifndef EXAMPLE_HPP_
-#define EXAMPLE_HPP_
-
-int MAC(int x, int y, int& sum);
-int Square(int x);
-
-#endif  // EXAMPLE_HPP_
--- test/ExampleTests.cc
+++ test/ExampleTests.cc
@@ -1,43 +1,23 @@
 // Copyright: 2020 -- Jared Leach
 #include <gtest/gtest.h>
-#include "../Example.hpp"
-#include "../Day14.hpp"
+#include "../include/Day14.hpp"
 
 TEST(ExampleTests, DemonstrateGTestMacros) {
     EXPECT_TRUE(true);
 }
 
-TEST(ExampleTests, MAC) {
-    int x =  42;
-    int y = 16;
-    int sum = 100;
-    int oldSum = sum;
-    int expectedNewSum = oldSum + x * y;
-
-    EXPECT_EQ(
-        expectedNewSum,
-        MAC(x, y, sum));
-    EXPECT_EQ(
-        expectedNewSum, sum);
-}
-
-TEST(ExampleTests, Square) {
-    int x = 100;
-    EXPECT_EQ(x, Square(10));
-}
-
 TEST(Day14, Input) {
-    InputParse input("/home/jared/Documents/GitHub/Advent2020/day14.txt");
+    InputParse input("/home/jared/Documents/GitHub/Advent2020/input/day14.txt");
     EXPECT_EQ(13902, input.input.size()) << "File read correctly";
 }
 
 TEST(Day14, Commands) {
-    InputParse input("/home/jared/Documents/GitHub/Advent2020/day14.txt");
+    InputParse input("/home/jared/Documents/GitHub/Advent2020/input/day14.txt");
     EXPECT_EQ(580, input.commands.size()) << "File read correctly";
 }
 
 TEST(Day14, Mask) {
-    InputParse input("/home/jared/Documents/GitHub/Advent2020/day14.txt");
+    InputParse input("/home/jared/Documents/GitHub/Advent2020/input/day14.txt");
     DockingData d(input.commands);
     EXPECT_EQ(580, d.commands.size());
     d.executeCommand();
